2da77d338a45204ad2db27f549d7177163baed4f
lvnana1206-hub <lvnana1206@gmail.com>
2026-08-11 17:58:32 +0800

    add analysis/__init__.py

---
+++ analysis/__init__.py
@@ -0,0 +1,6 @@
+"""经营分析引擎：输出结构化 JSON。"""
+from .daily_analysis import daily_analysis
+from .weekly_analysis import weekly_analysis
+from .monthly_analysis import monthly_analysis
+from .quarterly_analysis import quarterly_analysis
+from .insight_engine import generate_insights
