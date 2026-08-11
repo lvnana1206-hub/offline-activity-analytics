2013463c34bb23cde279c41fdb15cb316f0eb9c4
lvnana1206-hub <lvnana1206@gmail.com>
2026-08-11 17:58:39 +0800

    add api/__init__.py

---
+++ api/__init__.py
@@ -0,0 +1,8 @@
+"""Flask API 服务层。"""
+from .dashboard_api import dashboard_bp
+from .activity_api import activity_bp
+from .store_api import store_bp
+from .dealer_api import dealer_bp
+from .product_api import product_bp
+from .insight_api import insight_bp
+from .compat import compat_bp
